108c889f661b8a3f61a98c7e092a83babdae0d13
Alexey Motorin <97784424+AlexeyMotorin@users.noreply.github.com>
2024-03-04 14:33:26 +0300

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Алексей Моторин on 04.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
